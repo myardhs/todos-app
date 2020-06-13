6ae747ec075ce03c400d975a8838af2a7f7d00ef
Muh. Yasin Ardhi Sawal <muh.yasin.ardhi.sawal@gmail.com>
2020-06-13 19:59:28 +0800

    Updates

---
+++ precache-manifest.0593bca2c9021e60dcc4227eac9ca0e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69e00bfb180526f6c343",
+    "url": "/todos-app/static/css/main.92a8c167.chunk.css"
+  },
+  {
+    "revision": "69e00bfb180526f6c343",
+    "url": "/todos-app/static/js/main.69e00bfb.chunk.js"
+  },
+  {
+    "revision": "06de82619331a440d5d4",
+    "url": "/todos-app/static/js/1.06de8261.chunk.js"
+  },
+  {
+    "revision": "2629e9917dba5e498fbd",
+    "url": "/todos-app/static/js/runtime~main.2629e991.js"
+  },
+  {
+    "revision": "c4a508e31f30a07d081617207bbe5eca",
+    "url": "/todos-app/index.html"
+  }
+];
